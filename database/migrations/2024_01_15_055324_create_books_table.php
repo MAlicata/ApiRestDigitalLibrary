db1af4e1e7a369f5e8b7d853b87fc3a15e361012
Matias Alicata <matiasalicata@gmail.com>
2024-01-15 03:18:08 -0300

    add field in books table

--- database/migrations/2024_01_15_055324_create_books_table.php
+++ database/migrations/2024_01_15_055324_create_books_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->year('publication_year');
+            $table->integer('pages');
             $table->timestamps();
         });
     }
