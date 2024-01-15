d295750b1a18314dca4997d7c27c9ae8711b19a9
Matias Alicata <matiasalicata@gmail.com>
2024-01-15 12:32:30 -0300

    add routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix'=>'v1'], function(){
+    Route::apiResource('books',BooksController::class);
+    Route::apiResource('reviews',ReviewController::class);
+
+});
